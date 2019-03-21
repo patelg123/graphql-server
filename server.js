9e1538e53c3ae493c6d145f13298ff74e5b16462
Girish Patel <patelg123@gmail.com>
2019-03-21 16:42:15 +0000

    Seperated the data to a data file

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var express_graphql = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const express_graphql = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+var coursesData = require('./data.js');
+
 // GraphQL schema
-var schema = buildSchema(`
+const schema = buildSchema(`
     type Query {
         course(id: Int!): Course
         courses(topic: String): [Course]
@@ -16,32 +19,13 @@ var schema = buildSchema(`
         url: String
     }
 `);
-var coursesData = [
-    {
-        id: 1,
-        title: 'The Complete Node.js Developer Course',
-        author: 'Andrew Mead, Rob Percival',
-        description: 'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs/'
-    },
-    {
-        id: 2,
-        title: 'Node.js, Express & MongoDB Dev to Deployment',
-        author: 'Brad Traversy',
-        description: 'Learn by example building & deploying real-world Node.js applications from absolute scratch',
-        topic: 'Node.js',
-        url: 'https://codingthesmartway.com/courses/nodejs-express-mongodb/'
-    },
-    {
-        id: 3,
-        title: 'JavaScript: Understanding The Weird Parts',
-        author: 'Anthony Alicea',
-        description: 'An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
-        topic: 'JavaScript',
-        url: 'https://codingthesmartway.com/courses/understand-javascript/'
-    }
-]
+
+// Maps Query to below '....' functions
+var root = {
+    course: getCourse,
+    courses: getCourses
+};
+
 var getCourse = function(args) {
     var id = args.id;
     return coursesData.filter(course => {
@@ -56,10 +40,7 @@ var getCourses = function(args) {
         return coursesData;
     }
 }
-var root = {
-    course: getCourse,
-    courses: getCourses
-};
+
 // Create an express server and a GraphQL endpoint
 var app = express();
 app.use('/graphql', express_graphql({
